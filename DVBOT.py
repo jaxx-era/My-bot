389cacaf3b47610f1f1c59e379c203f50a037fb0
jaxx-era <koenn.jhon@gmail.com>
2025-11-07 21:12:41 +0530

    Update DVBOT.py

--- DVBOT.py
+++ DVBOT.py
@@ -30,7 +30,7 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
 def main() -> None:
     tg_token = os.getenv("TELEGRAM_BOT_TOKEN")
     app = ApplicationBuilder().token(tg_token).build()
-    app.add_handler(CommandHandler("start", start)) app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+    app.add_handler (CommandHandler ("start", start) ) app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     app.run_polling()
 
 if __name__ == '__main__':
